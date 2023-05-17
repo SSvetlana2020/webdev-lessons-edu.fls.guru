61c5569aa40a7958f6c9c3b1a8f169014accdaf1
Web Deva <61093188+SSvetlana2020@users.noreply.github.com>
2023-05-18 01:22:21 +0300

    Add files via upload
    16 lesson added mob

--- lesson_16/js/script.js
+++ lesson_16/js/script.js
@@ -4,3 +4,16 @@ window.addEventListener('DOMContentLoaded', function() {
       input.placeholder += ' *';
     });
 });
+
+// Отримуємо посилання на поле введення
+var input = document.getElementById("state-01");
+
+// Зупиняємо анімацію плейсхолдера при отриманні фокусу на полі введення
+input.addEventListener("focus", function() {
+  input.style.animation = "none";
+});
+
+// Відновлюємо анімацію плейсхолдера при втраті фокусу на полі введення
+input.addEventListener("blur", function() {
+  input.style.animation = "placeholder-blink 2s linear infinite";
+});
